7bd7a12384ec4a9536d0a2327392b5f6056dc986
[...Tomáš] <81377960+Tomik5421@users.noreply.github.com>
2022-06-26 10:41:20 +0200

    Update fxmanifest.lua

--- FPRP_Medicaments/fxmanifest.lua
+++ FPRP_Medicaments/fxmanifest.lua
@@ -1,24 +1,20 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 game 'gta5'
 Author 'Created by: [...𝓣𝓸𝓶𝓪𝓼]#7113'
-
-server_scripts {
-    '@async/async.lua',
-    '@mysql-async/lib/MySQL.lua',
-    '@es_extended/locale.lua',
-    'config.lua',
-    '**/server/*.lua'
-}
-
-client_scripts {
-    '@es_extended/locale.lua',
-    'cs.lua',
-    'config.lua',
-    '**/client/*.lua'
-}
 
 dependencies {
     'es_extended',
     'mythic_progbar'
 }
 
+shared_scripts {
+    'config.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
