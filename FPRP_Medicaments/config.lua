c1bb930fb6fc76fb641f9df4f55347e49725ac2e
[...Tomáš] <81377960+Tomik5421@users.noreply.github.com>
2022-06-26 10:40:51 +0200

    Update config.lua

--- FPRP_Medicaments/config.lua
+++ FPRP_Medicaments/config.lua
@@ -16,7 +16,7 @@ Locales = {
 	['you_used_paralen']      = 'Snědl si paralen',
 	['you_used_endiaron']      = 'Snědl si endiaron',
 	['you_used_xanax']      = 'Snědl si xanax',
-	['you_used_diazepam']      = 'v si diazepam',
+	['you_used_diazepam']      = 'Snědl si diazepam',
 	['you_used_aspirin']      = 'Snědl si aspirin',
 	['you_used_poison']      = 'Snědl si záhadný prášek'
 }
@@ -41,50 +41,57 @@ Config.PropCoords = { x = 0.13, y = 0.04, z = 0.0 }		-- degault : { x = 0.13, y 
 Config.Rotation = { x = -58.0, y = 0.0, z = 0.0 }		-- degault : { x = -58.0, y = 0.0, z = 0.0 }
 
 
-------------------------------------------------------------------------------
--------------------------------- ITEMS CONFIG --------------------------------
-------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
+-------------------------------- MEDICAMENT ITEMS CONFIG --------------------------------
+-----------------------------------------------------------------------------------------
 
 
 -------------------------------- IBALGIN ITEM --------------------------------
 Config.IbalginItem = "ibalgin"
 Config.IbalginProgressBar = "Bereš si ibalgin"
+Config.ProgressBarDurationIbalgin = 2500
 Config.IbalginHP = 50
 Config.IbalginRemoveAfterUse = true
 
 -------------------------------- PARALEN ITEM --------------------------------
 Config.ParalenItem = "paralen"
 Config.ParalenProgressBar = "Bereš si paralen"
+Config.ProgressBarDurationParalen = 2500
 Config.ParalenHP = 40
 Config.ParalenRemoveAfterUse = true
 
 -------------------------------- DIAZEPAM ITEM --------------------------------
 Config.DiazepamItem = "diazepam"
 Config.DiazepamProgressBar = "Bereš si diazepam"
+Config.ProgressBarDurationDiazepam = 2500
 Config.DiazepamHP = 30
 Config.DiazepamRemoveAfterUse = true
 
 -------------------------------- ASPIRIN ITEM --------------------------------
 Config.AspirinItem = "aspirin"
 Config.AspirinProgressBar = "Bereš si aspirin"
+Config.ProgressBarDurationAspirin = 2500
 Config.AspirinHP = 25
 Config.AspirinRemoveAfterUse = true
 
 -------------------------------- XANAX ITEM --------------------------------
 Config.XanaxItem = "xanax"
 Config.XanaxProgressBar = "Bereš si xanax"
+Config.ProgressBarDurationXanax = 2500
 Config.XanaxHP = 20
 Config.XanaxRemoveAfterUse = true
 
 -------------------------------- ENDIARON ITEM --------------------------------
 Config.EndiaronItem = "endiaron"
 Config.EndiaronProgressBar = "Bereš si endiaron"
+Config.ProgressBarDurationEndiaron = 2500
 Config.EndiaronHP = 10
 Config.EndiaronRemoveAfterUse = true
 
 -------------------------------- POISON ITEM --------------------------------
 Config.PoisonItem = "poison"
 Config.PoisonProgressBar = "Bereš si záhadný prášek"
+Config.ProgressBarDurationPoison = 2500
 Config.PoisonRemoveAfterUse = true
 Config.PoisonDeathTime = 120000 -- (2 minute) -- the time after which the player dies
 
